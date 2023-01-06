2415299f8a49bb6a215f2ff4f490741c5419f634
TheSilvaProf <fabio01@protonmail.ch>
2023-01-06 12:28:58 -0500

    input na pratica

---
+++ uso_input.py
@@ -0,0 +1,8 @@
+name = input("Digite o seu nome: ")
+age = int(input("Qual a sua idade?: "))
+height = float(input("Qual a sua altura?: "))
+
+print("Eaee "+name+"!")
+print("Vc tem "+str(age)+" anos de idade.")
+print("Vc tem "+str(height)+" cm de altura.")
+
